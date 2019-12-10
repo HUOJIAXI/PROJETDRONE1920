8e7c4466e4978a3930a2c2229c9828ce206fdf59
HUOJIAXI <JasonHuoeclille@gmail.com>
2019-12-10 22:03:26 +0100

    fix bug

--- TraitementDImage/Local_traite_V2.py
+++ TraitementDImage/Local_traite_V2.py
@@ -20,12 +20,12 @@ def read_img(path=img_path):
 	return img+1/255
 
 def green_style_v2(image):
-    img=image[:,:,1]/(image[:,:,1]+image[:,:,2]+image[:,:,0])
-    img=(img-np.min(img))/(np.max(img)-np.min(img))
+	img=image[:,:,1]/(image[:,:,1]+image[:,:,2]+image[:,:,0])
+	img=(img-np.min(img))/(np.max(img)-np.min(img))
 	img = cv2.GaussianBlur(img,(5,5),0) 
-    cv2.imshow('green', img)
-    cv2.waitKey()
-    return img
+	cv2.imshow('green', img)
+	cv2.waitKey()
+	return img
 
 def man_seuillage(image,thresh=0.4):
     dst = (image >= thresh) * 1.0
