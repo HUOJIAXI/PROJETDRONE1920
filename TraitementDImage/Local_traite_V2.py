8a65692ba7897b9bf6947f1620f16075fe2a7e41
HUOJIAXI <JasonHuoeclille@gmail.com>
2019-12-11 00:15:53 +0100

    Régler la base de donnée

--- TraitementDImage/Local_traite_V2.py
+++ TraitementDImage/Local_traite_V2.py
@@ -6,6 +6,10 @@ from skimage import filters
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import sys
+import datetime
+from time import sleep
+import MySQLdb
 
 img_path='/Users/huojiaxi/Desktop/Unknown.jpeg'
 
@@ -50,10 +54,25 @@ def compter_pourcentage(image,area=0):
 	pourcent = np.round(area/(height*width)*100, 3)
 	return pourcent
 
+def stockage(pourcent):
+    conn = MySQLdb.connect(
+    host = '127.0.0.1',
+    port = 3306,
+    user = 'pi',
+    passwd = 'projet2019',
+    db = 'PROJETDRONE'
+    )
+    
+    cur = conn.cursor()
+    cur.execute("insert into COUVERTVEGETAL values ('%f',now())" % (pourcent))
+    cur.close()
+    conn.commit()
+    conn.close()
 
 if __name__=="__main__":
     img=read_img()
     g_img=green_style_v2(img)
     a_img=auto_seuillage(g_img)
     pourcent=compter_pourcentage(a_img)
+    stockage(pourcent)
     print('Couverture végétale:', pourcent,'%') 
