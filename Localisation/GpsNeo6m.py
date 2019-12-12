30e42f19630af67992a915a11b4c90694c4cf039
HUOJIAXI <JasonHuoeclille@gmail.com>
2019-12-12 20:46:28 +0100

    make dir

---
+++ Localisation/GpsNeo6m.py
@@ -0,0 +1,16 @@
+import serial
+import pynmea2
+def parseGPS(str):
+    if str.find('GGA') > 0:
+        msg = pynmea2.parse(str)
+        print "Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude:%s %s" %(msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)
+
+    latitude = msg.lat
+    altitude = msg.altitude
+    altitude = msg.altitude
+    
+serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+
+while True:
+    str = serialPort.readline()
+    parseGPS(str)
