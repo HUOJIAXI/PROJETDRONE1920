61ff499de539455415b77d747ea462ecda3f928a
HUOJIAXI <JasonHuoeclille@gmail.com>
2019-12-13 22:21:59 +0000

    Modifier Gps

--- GpsNeo6m.py
+++ GpsNeo6m.py
@@ -1,16 +1,20 @@
 import serial
+import string
 import pynmea2
-def parseGPS(str):
-    if str.find('GGA') > 0:
-        msg = pynmea2.parse(str)
-        print "Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude:%s %s" %(msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)
-
-    latitude = msg.lat
-    altitude = msg.altitude
-    altitude = msg.altitude
-    
-serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 
 while True:
-    str = serialPort.readline()
-    parseGPS(str)
+    port="/dev/ttyAMA0"
+    ser = serial.Serial(port, 9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata = ser.readline()
+    
+   # if newdata[0:6] == "$GPRMC":
+
+    if newdata.find('GGA')>0: 
+        newmsg=pynmea2.parse(newdata)
+        lat = newmsg.latitude
+        lng = newmsg.longitude
+        alt = newmsg.altitude
+        
+        gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)+"Altitude" + str(alt)
+        print(gps)
