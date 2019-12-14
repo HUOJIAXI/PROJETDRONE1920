052a4c771a4cf77be8986b413c5764aee8f5bc6e
HUOJIAXI <JasonHuoeclille@gmail.com>
2019-12-14 17:48:45 +0000

    Automatique execution

--- Localisation/GpsNeo6m.py
+++ Localisation/GpsNeo6m.py
@@ -2,19 +2,20 @@ import serial
 import string
 import pynmea2
 
-while True:
-    port="/dev/ttyAMA0"
-    ser = serial.Serial(port, 9600, timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
-    newdata = ser.readline()
-    
-   # if newdata[0:6] == "$GPRMC":
-
-    if newdata.find('GGA')>0: 
-        newmsg=pynmea2.parse(newdata)
-        lat = newmsg.latitude
-        lng = newmsg.longitude
-        alt = newmsg.altitude
+def get():
+    while True:
+        port="/dev/ttyAMA0"
+        ser = serial.Serial(port, 9600, timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata = ser.readline()
         
-        gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)+"Altitude" + str(alt)
-        print(gps)
+       # if newdata[0:6] == "$GPRMC":
+
+        if newdata.find('GGA')>0: 
+            newmsg=pynmea2.parse(newdata)
+            lat = newmsg.latitude
+            lng = newmsg.longitude
+            alt = newmsg.altitude
+            
+            #gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)+"Altitude" + str(alt)
+            return int(alt)
--- projet_test.sh
+++ projet_test.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
-python /home/pi/projetdronetech/TraitementDImage/Local_traite_V2.py > /dev/null 2>&1 &
-python /home/pi/projetdronetech/Pollutiondair/aqi_single.py.1 > /dev/null 2>&1 &
-python /home/pi/projetdronetech/TSL2561/Python/lumiere.py > /dev/null 2>&1 &
-python /home/pi/projetdronetech/Adafruit_AMG88xx_python/examples/pixels_test.py > /dev/null 2>&1 &
+echo -n "Veuillez saisir le maxi nombre d'executions: "
+read num
+echo -n "L'intervalle entre les executions: "
+read temp
+echo -n "Veuillez saisir l'altitude a laquelle commencer les tests: "
+read hgt
+
+int=0
+
+while true;
+do
+	cd /home/pi/projetdronetech/Localisation/
+	alt=`python -c 'import GpsNeo6m; print GpsNeo6m.get()'`
+	echo "L'altitude actuelle : $alt"
+	if [ $alt -gt $hgt ]
+	then
+		python /home/pi/projetdronetech/TraitementDImage/Local_traite_V2.py
+		sleep 0.1
+		python /home/pi/projetdronetech/Pollutiondair/aqi_single.py.1
+		sleep 0.1
+		python /home/pi/projetdronetech/TSL2561/Python/lumiere.py 
+		sleep 0.1
+		python /home/pi/projetdronetech/Adafruit_AMG88xx_python/examples/pixels_test.py
+		sleep 0.1
+		let "int=int+1"
+		echo "$int fois"
+	
+		sleep $temp
+	
+		if [ $int -eq $num ]
+		then
+			exit 0
+		fi
+	fi
+done
+	
