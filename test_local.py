d73a85c94f2f7928d85545bea59c2a6e7f09d5e4
HUOJIAXI <JasonHuoeclille@gmail.com>
2019-12-15 00:38:34 +0100

    tester localisation

---
+++ test_local.py
@@ -0,0 +1,26 @@
+import serial
+import string
+import pynmea2
+
+def main():
+    while True:
+        port="/dev/ttyAMA0"
+        ser = serial.Serial(port, 9600, timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata = ser.readline()
+        
+       # if newdata[0:6] == "$GPRMC":
+
+        if newdata.find('GGA')>0: 
+            newmsg=pynmea2.parse(newdata)
+            lat = newmsg.latitude
+            lng = newmsg.longitude
+            alt = newmsg.altitude
+
+            return lat,lng,alt
+
+
+if __name__ == '__main__':
+    lat,lng,alt = main()
+    gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)+"Altitude" + str(alt)
+    print(gps)
